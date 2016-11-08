2e7ed6cdb8ae31e03ba79881538f61e80381f816
“Matthew <matthew.hinea@gmail.com>
2016-11-07 16:40:52 -0800

    working email v phone number selector

--- javascripts/contacts_widget.js
+++ javascripts/contacts_widget.js
@@ -2,7 +2,7 @@ var CONTACTS_WIDGET = CONTACTS_WIDGET || {};
 
 var widget = document.getElementById('contacts-widget');
 var widgetTable = widget.querySelector('table');
-var tableRows = widgetTable.querySelector('tbody').children;
+var _tableRows = widgetTable.querySelector('tbody').children;
 
 var _addClass = function(el, className) {
     if (el.classList) {
@@ -15,17 +15,17 @@ var _addClass = function(el, className) {
 
 CONTACTS_WIDGET.colorTableRows = function() {
 
-  for (var i = 0; i < tableRows.length; i++) {
+  for (var i = 0; i < _tableRows.length; i++) {
     if (i % 2 === 0) {
-      _addClass(tableRows[i], "even-row");
+      _addClass(_tableRows[i], "even-row");
     } else {
-      _addClass(tableRows[i], "odd-row");
+      _addClass(_tableRows[i], "odd-row");
     }
   }
 }
 
 CONTACTS_WIDGET.addOrbs = function() {
-  // for (var i = 0; i < tableRows.length; i++) {
+  // for (var i = 0; i < _tableRows.length; i++) {
   //   // get first TD
   //   // insert div with a class of "orb green/yellow/red" into innerHTML before text
   // }
@@ -33,10 +33,20 @@ CONTACTS_WIDGET.addOrbs = function() {
 
 CONTACTS_WIDGET.switchInfoType = function() {
   console.log(selector.value);
+  var _populateEmailAddresses = function() {
+    for (var i = 0; i < _tableRows.length; i++) {
+      _tableRows[i].children[1].innerHTML = "some email";
+    }
+  };
+  var _populatePhoneNumbers = function() {
+    for (var i = 0; i < _tableRows.length; i++) {
+      _tableRows[i].children[1].innerHTML = "323-555-1234";
+    }
+  };
   if (selector.value === "Email address") {
-    // populate tree with email addresses
+    _populateEmailAddresses();
   } else if (selector.value === "Phone number") {
-    // populate tree with phone numbers
+    _populatePhoneNumbers();
   }
 }
 
